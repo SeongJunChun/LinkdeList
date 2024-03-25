7a636984699413384987b956625758ec4e491d26
SeongJunChun <csj991210@naver.com>
2024-03-25 17:37:45 +0900

    코드 컨벤션 수정

--- src/Node.java
+++ src/Node.java
@@ -1,11 +1,13 @@
 public class Node<T> {
-    private T data;
-    public Node next;
-    public Node(T t){
-        data=t;
-        next=null;
-    }
-    public T getData(){
-        return data;
-    }
+	private T data;
+	public Node next;
+
+	public Node(T t) {
+		data = t;
+		next = null;
+	}
+
+	public T getData() {
+		return data;
+	}
 }
--- src/Stack.java
+++ src/Stack.java
@@ -1,23 +1,23 @@
-public class Stack extends LinkedList{
-    public void pull(Object data){
-        Node node = new Node<>(data);
-        if(tail==null){
-            tail=node;
-            head=node;
-        }
-        else{
-            tail.next=node;
-            tail=node;
-        }
-    }
-    public Object pop(){
-        Node node = head;
-        while(node.next.next!=null){
-            node=node.next;
-        }
-        Object object = node.next.getData();
-        node.next=null;
-        tail=node;
-        return object;
-    }
+public class Stack extends LinkedList {
+	public void pull(Object data) {
+		Node node = new Node<>(data);
+		if (tail == null) {
+			tail = node;
+			head = node;
+		} else {
+			tail.next = node;
+			tail = node;
+		}
+	}
+
+	public Object pop() {
+		Node node = head;
+		while (node.next.next != null) {
+			node = node.next;
+		}
+		Object object = node.next.getData();
+		node.next = null;
+		tail = node;
+		return object;
+	}
 }
