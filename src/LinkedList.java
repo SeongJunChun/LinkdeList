bd0c4cad16e09e45632ae518b54c846d21a28804
SeongJunChun <csj991210@naver.com>
2024-03-25 17:38:41 +0900

    Iterator 구현

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -1,51 +1,66 @@
 import java.util.Iterator;
 
-public class LinkedList{
-    protected Node tail=null;
-    protected Node head=null;
+public class LinkedList implements Iterable<Object> {
+	protected Node tail = null;
+	protected Node head = null;
 
-    public void add(Object data){
-        Node node = new Node<>(data);
-        if(tail==null){
-            tail=node;
-            head=node;
-        }
-        else{
-            tail.next=node;
-            tail=node;
-        }
+	public void add(Object data) {
+		Node node = new Node<>(data);
+		if (tail == null) {
+			tail = node;
+			head = node;
+		} else {
+			tail.next = node;
+			tail = node;
+		}
 
-    }
-    public Object get(int index){
-        Node node=head;
-        for(int i=0;i<index;i++){
-            node=node.next;
-        }
-        return node.getData();
-    }
-    public void delete(int index){
-        if(index==0){
-            head=head.next;
-        }
-        else{
-            Node node=head;
-            for(int i=0;i<index-1;i++){
-                node=node.next;
-            }
-            if(node.next.next==null){
-                node.next=null;
-                tail=node;
-            }
-            else{
-                node.next=node.next.next;
-            }
+	}
 
-        }
+	public Object get(int index) {
+		Node node = head;
+		for (int i = 0; i < index; i++) {
+			node = node.next;
+		}
+		return node.getData();
+	}
 
-    }
+	public void delete(int index) {
+		if (index == 0) {
+			head = head.next;
+		} else {
+			Node node = head;
+			for (int i = 0; i < index - 1; i++) {
+				node = node.next;
+			}
+			if (node.next.next == null) {
+				node.next = null;
+				tail = node;
+			} else {
+				node.next = node.next.next;
+			}
 
+		}
 
+	}
 
+	@Override
+	public Iterator<Object> iterator() {
+		return new LinkedListIterator();
+	}
 
+	private class LinkedListIterator implements Iterator<Object> {
+		private Node current = head;
 
+		@Override
+		public boolean hasNext() {
+			return current != null;
+		}
+
+		@Override
+		public Object next() {
+			Object data = current.getData();
+			current = current.next;
+			return data;
+		}
+	}
 }
--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,40 @@
 
 public class Main {
-    public static void main(String[] args) {
-        Queue list = new Queue();
-        list.pull('a');
-        list.pull('b');
-        list.pull('c');
-        list.pop();
-        list.pull('f');
-        System.out.println(list.get(0));
-        for(Queue q : list){
+	public static void main(String[] args) {
+		System.out.println("큐 구현 ----------------------------");
+		Queue Q = new Queue();
+		Q.pull('a');
+		Q.pull('b');
+		Q.pull('c');
+		Q.pop();
+		Q.pull('d');
+		for (Object q : Q) {
+			System.out.println(q);
+		}
 
-        }
+		System.out.println("스택 구현 ----------------------------");
 
-    }
+		Stack stack = new Stack();
+		stack.pull(1);
+		stack.pull(2);
+		stack.pop();
+		stack.pull(3);
+		stack.pull(4);
+		for (Object s : stack) {
+			System.out.println(s);
+		}
+
+		System.out.println("리스트 구현 ----------------------------");
+		LinkedList list = new LinkedList();
+		list.add("Red");
+		list.add("Orange");
+		list.add("Yellow");
+		list.add("Black");
+		list.delete(1);
+		list.get(2);
+		for (Object l : list) {
+			System.out.println(l);
+		}
+
+	}
 }
